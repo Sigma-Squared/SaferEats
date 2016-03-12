bb0890039ac3649d47605e6c0a2ed9fbbeca987f
Sigma-Squared <chamu.rajasekera@gmail.com>
2016-03-12 11:07:00 -0500

    added csv parsing

--- data_loader.js
+++ data_loader.js
@@ -1,5 +1,42 @@
+/// <reference path="./typings/main.d.ts" />
+'use strict';
+
 const restaurant = require('./restaurant.js');
+const fs = require('fs');
+const csv_stream = require('csv-stream');
 
-const dat = [new restaurant('a','b',[0,0]), new restaurant('ayy', 'lmao',[4,20])]
+function load_data(callback) {
+    let dat = [];
+    let options = { columns: ['name','program_identifier','inspection_date','description','address','city','zip_code',
+    'phone','longitude','latitude','inspection_business_name','inspection_type','inspection_score','inspection_result',
+    'inspection_closed_business','violation_type','violation_description','violation_points','business_ID',
+    'inspection_serial_num','violation_record_ID','location'] };
 
-module.exports = dat;
+    let prev_rest = null;
+    let mystream = csv_stream.createStream(options);
+    console.log('Starting load');
+    fs.createReadStream('small_data.csv').pipe(mystream)
+        .on('error', function(err) {
+            throw err;
+        })
+        .on('data', function(data) {
+            if (prev_rest && data.business_ID == prev_rest.id) {
+                //add violation
+                prev_rest.add_violation(data.inspection_date, data.inspection_type, data.violation_type, data.inspection_score,
+                data.inspection_result, data.inspection_closed_business, data.violation_description, data.violation_points);
+            }
+            else {
+                let new_restaurant  = new restaurant(data.name, data.address, data.city, 
+                                        [Number(data.longitude), Number(data.latitude)], data.phone || undefined, data.business_ID);
+                new_restaurant.add_violation(data.inspection_date, data.inspection_type, data.violation_type, data.inspection_score,
+                data.inspection_result, data.inspection_closed_business, data.violation_description, data.violation_points);
+                dat.push(new_restaurant);
+                prev_rest = new_restaurant;
+            }
+        })
+        .on('end', function() {
+            console.log('Loading finished.')
+            callback(dat);
+        });
+}
+module.exports = load_data;
--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 // Ignore this, it's just for VS Code.
-/// <reference path="typings/main/ambient/express/index.d.ts" />
-/// <reference path="typings/main/ambient/node/index.d.ts" />
+/// <reference path="typings/main.d.ts" />
 
 'use strict';
 const express = require('express');
+const load_data = require('./data_loader.js');
 
-// ! require() is synchronous so this works.
-console.log('Loading data into memory...');
-const data = require('./data_loader.js');
-console.log('Finished.');
+function main(data) {
+    const port = 80;
+    let app = express();
 
-const port = 80;
-let app = express();
+    //example server
+    app.get('/api', function(req, res) {
+        let rest_array = [];
+        let query = req.query.name
+        console.log(`Doing linear search for restaurants with name ${query}`);
+        //simple linear search
+        for (let restaurant of data) {
+            if (restaurant.name == query)
+            rest_array.push(restaurant);  
+        }    
+        res.json(rest_array);
+    });
+    app.listen(port, function() {
+        console.log(`Server running on port ${port}`);
+    });
+}
 
-//example server
-app.get('/api', function(req, res) {
-    let rest_array = [];
-    let query = req.query.name
-    console.log(`Doing linear search for restaurants with name ${query}`);
-    //simple linear search
-    for (let restaurant of data) {
-        if (restaurant.name == query)
-           rest_array.push(restaurant);  
-    }    
-    res.json(rest_array);
-});
-app.listen(port, function() {
-    console.log(`Server running on port ${port}`);
-});
+load_data(main);
