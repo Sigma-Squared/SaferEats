cf3ded8f1e0f0b41b8e2aaebfd5ad0270b8e964d
Sigma-Squared <chamu.rajasekera@gmail.com>
2016-03-13 12:43:40 -0400

    switched to linux

--- data_loader.js
+++ data_loader.js
@@ -19,11 +19,23 @@ function load_data(callback) {
     const fname = 'small_data.csv';
     const total_lines = { 'small_data.csv': 150, 'raw_data.csv': 245531 };
     let line_count = 0;
+    let n_restaurants = 0;
+    let n_violations = 0;
+    
+    let bounds = { min_long: 999, min_lat:999, max_long:0, max_lat:0 }
+    function update_bounds(rest) {
+        let x = rest.location[0], y = rest.location[1];
+        if (x<bounds.min_long) bounds.min_long = x;
+        if (x>bounds.max_long) bounds.max_long = x;
+        if (y<bounds.min_lat) bounds.min_lat = y;
+        if (y>bounds.max_lat) bounds.max_lat = y;
+    }
     fs.createReadStream(fname).pipe(mystream)
         .on('error', function(err) {
             throw err;
         })
         .on('data', function(data) {
+            n_violations++;
             if (prev_rest && data.name == prev_rest.name) {
                 //add violation
                 prev_rest.add_violation(data.inspection_date, data.inspection_type, data.violation_type, data.inspection_score,
@@ -31,8 +43,10 @@ function load_data(callback) {
             }
             else {
                 //add restaurant and violation
+                n_restaurants++;
                 let new_restaurant  = new restaurant(data.name, data.address, data.city, 
                                         [Number(data.longitude), Number(data.latitude)], data.phone || undefined, data.business_ID);
+                update_bounds(new_restaurant);                   
                 new_restaurant.add_violation(data.inspection_date, data.inspection_type, data.violation_type, data.inspection_score,
                 data.inspection_result, data.inspection_closed_business, data.violation_description, data.violation_points);
                 dat.push(new_restaurant);
@@ -42,8 +56,8 @@ function load_data(callback) {
             process.stdout.write(`\r${(((++line_count)/total_lines[fname])*100).toFixed(2)}%`)
         })
         .on('end', function() {
-            console.log('\nFinished.')
-            callback(dat);
+            console.log(`\nFinished. Loaded ${n_restaurants} restaurants and ${n_violations} violations.`)
+            callback(dat, bounds);
         });
 }
 module.exports = load_data;
--- server.js
+++ server.js
@@ -4,25 +4,39 @@
 'use strict';
 const express = require('express');
 const load_data = require('./data_loader.js');
-const algorithms = require('./algorithms.js')
+const algorithms = require('./algorithms.js');
+const quadtree = require('./simplequadtree.js');
 
-function main(data) {
+function main(data, bounds) {
     const port = 80;
     let app = express();
     app.set('json spaces', 3);
-    //example server
     app.get('/api', function(req, res) {
-        let query = req.query.name.toUpperCase();
-        console.log(`Doing binary search for name:"${query}"`);
-        let index =  algorithms.binaryIndexOf(query, data);
-        if (index != -1) {
-            res.json( data[index] );
-            console.log("Found.")
+        let name = req.query.name
+        let long = req.query.long;
+        let lat = req.query.lat;
+        if (name) {
+            name = name.toUpperCase();
+            console.log(`Doing binary search for name:"${name}"`);
+            let index =  algorithms.binaryIndexOf(name, data);
+            if (index != -1) {
+                res.json( data[index] );
+                console.log("Found.")
+            }
+            else {
+                res.json(null);
+                console.log("Not found.");
+            }
+        }
+        else if (long && lat) {
+            let loc = [Number(long), Number(lat)];
+            console.log(loc);
+            res.send('Not yet implemented');
         }
         else {
-            res.json(null);
-            console.log("Not found.");
+            res.send('Bad GET request.')
         }
+        
     });
     app.listen(port, function() {
         console.log(`Server running on port ${port}. (^C to exit)`);
