bac63e5bea505764036659fdb465abed94ab3170
Sigma-Squared <chamu.rajasekera@gmail.com>
2016-03-12 22:10:13 -0500

    fixed bug

--- server.js
+++ server.js
@@ -13,8 +13,16 @@ function main(data) {
     //example server
     app.get('/api', function(req, res) {
         let query = req.query.name.toUpperCase();
-        console.log(`Doing binary search for name:"${query}"`);  
-        res.json( data[algorithms.binaryIndexOf(query, data)] );
+        console.log(`Doing binary search for name:"${query}"`);
+        let index =  algorithms.binaryIndexOf(query, data);
+        if (index != -1) {
+            res.json( data[index] );
+            console.log("Found.")
+        }
+        else {
+            res.json(null);
+            console.log("Not found.");
+        }
     });
     app.listen(port, function() {
         console.log(`Server running on port ${port}. (^C to exit)`);
